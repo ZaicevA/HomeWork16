00c3a38c8c22abbf0fcb9b57c6121197fa6eddfe
Artur <arturrabbit3@gmail.com>
2020-05-26 02:25:29 +0300

    Добавьте файлы проекта.

---
+++ HomeWork16/HomeWork16.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <time.h>
+
+int main()
+{
+    const int n = 10;
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = i + j;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    time_t myTime = time(NULL);
+    tm myTm;
+    localtime_s(&myTm, &myTime);
+    auto day = myTm.tm_mday;
+    auto stringNumber = day % n;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[stringNumber][i];
+    }
+    std::cout << "String number is: " << stringNumber << ", sum is: " << sum;
+}
